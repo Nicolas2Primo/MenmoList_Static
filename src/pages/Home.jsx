582b3a8274937b76af7a4bf5388f23f9b5fd07e2
Nicolas Primo <nicolasprimo@gmail.com>
2023-02-01 13:54:38 -0300

    fixes onLoadedData

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
           }}
           autoPlay
           loop
-          onCanPlay={() => {
+          onLoadedData={() => {
             handleLoad();
           }}
           muted
