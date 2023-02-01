4b81a816411dd03a60903d352e74c550e0500d22
Nicolas Primo <nicolasprimo@gmail.com>
2023-02-01 13:59:13 -0300

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,6 +24,7 @@ const Home = () => {
             e.preventDefault();
           }}
           autoPlay
+          controls={false}
           loop
           onLoadedData={() => {
             handleLoad();
